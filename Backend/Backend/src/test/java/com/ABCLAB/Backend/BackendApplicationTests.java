d098d24909d1fb208fd0a5edf75d28eb78022c1a
judesukirthan <brucebannerjude@gmail.com>
2024-03-18 09:47:32 +0530

    first commit

---
+++ Backend/Backend/src/test/java/com/ABCLAB/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ABCLAB.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
